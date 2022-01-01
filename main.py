ddd9bf3e897c4be2c8589c5e617258ed7082a6d3
Aleksandr Petrov <firexel@gmail.com>
2022-01-01 12:44:49 +0000

    ported to tensorflow v2

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from sampler import WarpSampler
 from model import Model
 from tqdm import tqdm
 from util import *
+tf.compat.v1.disable_eager_execution()
+
+
 
 
 def str2bool(s):
@@ -36,51 +39,46 @@ f.close()
 
 dataset = data_partition(args.dataset)
 [user_train, user_valid, user_test, usernum, itemnum] = dataset
-num_batch = len(user_train) / args.batch_size
+num_batch = len(user_train) // args.batch_size
 cc = 0.0
 for u in user_train:
     cc += len(user_train[u])
-print 'average sequence length: %.2f' % (cc / len(user_train))
+print('average sequence length: %.2f' % (cc / len(user_train)))
 
 f = open(os.path.join(args.dataset + '_' + args.train_dir, 'log.txt'), 'w')
-config = tf.ConfigProto()
+config = tf.compat.v1.ConfigProto()
 config.gpu_options.allow_growth = True
 config.allow_soft_placement = True
-sess = tf.Session(config=config)
+sess = tf.compat.v1.Session(config=config)
 
 sampler = WarpSampler(user_train, usernum, itemnum, batch_size=args.batch_size, maxlen=args.maxlen, n_workers=3)
 model = Model(usernum, itemnum, args)
-sess.run(tf.initialize_all_variables())
+sess.run(tf.compat.v1.initialize_all_variables())
 
 T = 0.0
 t0 = time.time()
 
-try:
-    for epoch in range(1, args.num_epochs + 1):
+for epoch in range(1, args.num_epochs + 1):
+    print(f"epoch: {epoch}")
+    for step in tqdm(range(num_batch), total=num_batch, ncols=70, leave=False, unit='b'):
+        u, seq, pos, neg = sampler.next_batch()
+        auc, loss, _ = sess.run([model.auc, model.loss, model.train_op],
+                                {model.u: u, model.input_seq: seq, model.pos: pos, model.neg: neg,
+                                 model.is_training: True})
 
-        for step in tqdm(range(num_batch), total=num_batch, ncols=70, leave=False, unit='b'):
-            u, seq, pos, neg = sampler.next_batch()
-            auc, loss, _ = sess.run([model.auc, model.loss, model.train_op],
-                                    {model.u: u, model.input_seq: seq, model.pos: pos, model.neg: neg,
-                                     model.is_training: True})
+    if epoch % 20 == 0:
+        t1 = time.time() - t0
+        T += t1
+        print('Evaluating'),
+        t_test = evaluate(model, dataset, args, sess)
+        t_valid = evaluate_valid(model, dataset, args, sess)
+        print('')
+        print ('epoch:%d, time: %f(s), valid (NDCG@10: %.4f, HR@10: %.4f), test (NDCG@10: %.4f, HR@10: %.4f)' % (
+        epoch, T, t_valid[0], t_valid[1], t_test[0], t_test[1]))
 
-        if epoch % 20 == 0:
-            t1 = time.time() - t0
-            T += t1
-            print 'Evaluating',
-            t_test = evaluate(model, dataset, args, sess)
-            t_valid = evaluate_valid(model, dataset, args, sess)
-            print ''
-            print 'epoch:%d, time: %f(s), valid (NDCG@10: %.4f, HR@10: %.4f), test (NDCG@10: %.4f, HR@10: %.4f)' % (
-            epoch, T, t_valid[0], t_valid[1], t_test[0], t_test[1])
-
-            f.write(str(t_valid) + ' ' + str(t_test) + '\n')
-            f.flush()
-            t0 = time.time()
-except:
-    sampler.close()
-    f.close()
-    exit(1)
+        f.write(str(t_valid) + ' ' + str(t_test) + '\n')
+        f.flush()
+        t0 = time.time()
 
 f.close()
 sampler.close()
